ca7c8aa092b9cad4a219c37c01484d86576e8380
Amanda Leeson <amanda.leeson@westjet.com>
2019-09-18 17:21:43 -0600

    adding file attributes

--- myFileAnalyzer.py
+++ myFileAnalyzer.py
@@ -1,4 +1,5 @@
-# Python 3
+# Python 3.7
+# Running on macOS
 
 """
 1     File size
@@ -21,24 +22,38 @@
 import os, subprocess
 
 results = []
+filePermissions = [
+    {
+        'file type': [{'d':''}],
+        {
+            'owner': ''},{'group': ''}, {'others': ''}, {'alternateAccessMethod':['.':'security context, no alternate access method.']}]
+# d = the file type
+# rwx = owner permissions
 
-aFile = input('File to analyze (ie test.txt): ')
+aFile = input('\nFile to analyze (ie test.txt): ')
 
 # 1 Get the File size
-getFileSize = subprocess.run(["du", "-h", aFile], capture_output=True, text=True)
+getFileSize = subprocess.run(["du", "-h", aFile], capture_output=True, check=True, text=True)
 getFileSize = getFileSize.stdout
-getFileSize.replace(aFile, '')
-print("File Size:", getFileSize)
+
+# output formatting
+if aFile in getFileSize:
+    getFileSize = getFileSize.replace(aFile, '')
+print("\nFile Size:", getFileSize)
 results.append({'File Size':getFileSize})
 
 # 2 Get File Attributes (owner, group, permissions, timestamps, &c)
+getFileAttributes = subprocess.run(["ls", "-l", aFile], capture_output=True, check=True, text=True)
+fileAttributesList = getFileAttributes.stdout.split()
+print(fileAttributesList[0])
+#print("File Attributes:", getFileAttributes.stdout)
 
 # 3 Get extended File Attributes
 
 
 # 4 Get the File Type
 fileType = 'file {}'.format(aFile)
-getFileType = subprocess.run(["file", "-b", aFile], capture_output=True, text=True)
+getFileType = subprocess.run(["file", "-b", aFile], capture_output=True, check=True, text=True)
 print("File Type:", getFileType.stdout)
 results.append({'File Type':getFileType})
 
