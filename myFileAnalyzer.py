d70f6fd5de754966e08dc6f135e575b9c8b6ad9f
Amanda Leeson <amanda.leeson@westjet.com>
2019-10-16 10:01:49 -0600

    Got rid of 'attr' code Added 'main' method for myFileAnalyzer.py Created new functions for each step of file analysis process

--- myFileAnalyzer.py
+++ myFileAnalyzer.py
@@ -26,10 +26,24 @@
 16    What happens differently between the first and second times printf is called
 
 NOTE: All of these may not yeild results
-"""
-import os, subprocess
 
-results = []
+Helpful Commands:
+ls … file size and attributes
+lsattr … extended file attributes
+file … file type, may not be exact in all cases
+strings … seemingly-printable strings within file
+od and hexdump … print binary data in readable ways
+nm … file symbol information
+ldd … shared library information
+strace … trace system calls
+ltrace … trace library calls
+readelf … ELF executable information
+objdump … assembly code and more
+ndisasm … also disassembly, but better with unstructured binary code
+"""
+import os, subprocess, sys, argparse
+
+# Global Variables
 absoluteNumerics = {'0': 'No Permission',
                     '1': 'Execute',
                     '2': 'Write',
@@ -39,43 +53,45 @@ absoluteNumerics = {'0': 'No Permission',
                     '6':'Read+Write',
                     '7':'Read+Write+Execute'}
 
-aFile = input('\nFile to analyze (ie test.txt): ')
 
 # 1 Get the File size
-getFileSize = subprocess.run(["du", "-h", aFile], capture_output=True, check=True, text=True)
-getFileSize = getFileSize.stdout
+def get_FileSize(aFile):
+  aFile = aFile
+  getFileSize = subprocess.run(["du", "-h", aFile], capture_output=True, check=True, text=True)
+  getFileSize = getFileSize.stdout
 
-if aFile in getFileSize:
+  if aFile in getFileSize:
     getFileSize = getFileSize.replace(aFile, '')
-print("\n--- File Size ---\n", getFileSize)
-
+    print("\n--- File Size ---\n", getFileSize)
 
 # 2 Get File Attributes
 # Owner, Group, Timestamps
-getFileAttributes = subprocess.run(["ls", "-l", aFile], capture_output=True, check=True, text=True)
-fileAttributesList = getFileAttributes.stdout.split()
-ownerName = fileAttributesList[2]
-groupID = fileAttributesList[3]
-fileSizeBytes = fileAttributesList[4]
-lastModified_Month = fileAttributesList[5]
-lastModified_Day = fileAttributesList[6]
-lastModified_Hour = fileAttributesList[7]
+def get_FileAttributes(aFile):
+  getFileAttributes = subprocess.run(["ls", "-l", aFile], capture_output=True, check=True, text=True)
+  fileAttributesList = getFileAttributes.stdout.split()
+  ownerName = fileAttributesList[2]
+  groupID = fileAttributesList[3]
+  #fileSizeBytes = fileAttributesList[4]
+  lastModified_Month = fileAttributesList[5]
+  lastModified_Day = fileAttributesList[6]
+  lastModified_Hour = fileAttributesList[7]
 
-print("--- File Attributes ---")
-print("Owner Name:", ownerName)
-print("Group Name or ID:", groupID)
-print("Last Modified:", lastModified_Month, lastModified_Day, lastModified_Hour)
+  print("--- File Attributes ---")
+  print("Owner Name:", ownerName)
+  print("Group Name or ID:", groupID)
+  print("Last Modified:", lastModified_Month, lastModified_Day, lastModified_Hour)
 
 # Permissions
-getFilePermOctal = subprocess.run(["stat", "-c", "'%a %n'", aFile], capture_output=True, check=True, text=True)
-fileOctal = getFilePermOctal.stdout
-owner = fileOctal[1]
-group = fileOctal[2]
-other = fileOctal[3]
+def get_FilePermissions(aFile):
+  getFilePermOctal = subprocess.run(["stat", "-c", "'%a %n'", aFile], capture_output=True, check=True, text=True)
+  fileOctal = getFilePermOctal.stdout
+  owner = fileOctal[1]
+  group = fileOctal[2]
+  other = fileOctal[3]
 
-print("Owner Permissions:", absoluteNumerics[owner])
-print("Group Permissions:", absoluteNumerics[group])
-print("All Others Permissions:", absoluteNumerics[other])
+  print("Owner Permissions:", absoluteNumerics[owner])
+  print("Group Permissions:", absoluteNumerics[group])
+  print("All Others Permissions:", absoluteNumerics[other])
 
 # 3 Extended attributes (NOT WORKING ON SCHOOL COMP)
 # Trying to use lsattr: lsattr: Inappropriate ioctl for device While reading flags on a.out
@@ -88,64 +104,72 @@ print("All Others Permissions:", absoluteNumerics[other])
 #    print('None.')
 
 # 4 Get the File Type
-fileType = 'file {}'.format(aFile)
-getFileType = subprocess.run(["file", "-b", aFile], capture_output=True, check=True, text=True)
-print("\n--- File Type ---", '\n', getFileType.stdout)
+def get_FileType(aFile):
+  getFileType = subprocess.run(["file", "-b", aFile], capture_output=True, check=True, text=True)
+  print("\n--- File Type ---", '\n', getFileType.stdout)
 
 
 # 5 Interesting strings within the file
 # use option '-el' to get unicode strings
-getASCII = subprocess.run(["strings", "-a", aFile], capture_output=True, check=True, text=True)
-stringFileText = getASCII.stdout
-print(stringFileText, file=open("fileStrings.txt", "w"))
-print("\n--- Extracted ASCII Strings ---\n")
-print("FILE CREATED: fileStrings.txt")
+def get_FileStrings(aFile):
+  getASCII = subprocess.run(["strings", "-a", aFile], capture_output=True, check=True, text=True)
+  stringFileText = getASCII.stdout
+  print(stringFileText, file=open("fileStrings.txt", "w"))
+  print("\n--- Extracted ASCII Strings ---\n")
+  print("FILE CREATED: fileStrings.txt")
 
 
 # 6 Symbols Defined in the Executable
-getSymbols = subprocess.run(["nm", aFile], capture_output=True, check=True, text=True)
-symbolText = getSymbols.stdout
-print("\n--- Symbols Defined in Executable ---\n")
-print(symbolText)
+def get_Internal_File_Symbols(aFile):
+  getSymbols = subprocess.run(["nm", aFile], capture_output=True, check=True, text=True)
+  symbolText = getSymbols.stdout
+  print("\n--- Symbols Defined in Executable ---\n")
+  print(symbolText)
 
 # Get hexdump
-getHexDump = subprocess.run(["hexdump", "-C", aFile], capture_output=True, check=True, text=True)
-hexFileText = getHexDump.stdout
-print(hexFileText, file=open("hexDump.txt", "w"))
-print("\n--- HexDump ---\n")
-print("FILE CREATED: hexDump.txt\n")
+def get_FileHexdump(aFile):
+  getHexDump = subprocess.run(["hexdump", "-C", aFile], capture_output=True, check=True, text=True)
+  hexFileText = getHexDump.stdout
+  print(hexFileText, file=open("hexDump.txt", "w"))
+  print("\n--- HexDump ---\n")
+  print("FILE CREATED: hexDump.txt\n")
 
 
 # 7 Symbols Imported into the Executable
 # display only external symbols of executable
-getExternalSymbols = subprocess.run(["nm", "-g", aFile], capture_output=True, check=True, text=True)
-externalSymbols = getExternalSymbols.stdout
-print(externalSymbols, file=open("externalSymbols.txt", "w"))
-print("\n--- External Symbols ---\n")
-print("FILE CREATED: externalSymbols.txt\n")
+def get_External_File_Symbols(aFile):
+  getExternalSymbols = subprocess.run(["nm", "-g", aFile], capture_output=True, check=True, text=True)
+  externalSymbols = getExternalSymbols.stdout
+  print(externalSymbols, file=open("externalSymbols.txt", "w"))
+  print("\n--- External Symbols ---\n")
+  print("FILE CREATED: externalSymbols.txt\n")
 
 
 # 8 Required Libraries
-getReqLibraries = subprocess.run(["ldd", "-v", aFile], capture_output=True, check=True, text=True)
-print("\n--- Required Libraries ---\n")
-print(getReqLibraries.stdout)
+def get_File_Required_Libraries(aFile):
+  getReqLibraries = subprocess.run(["ldd", "-v", aFile], capture_output=True, check=True, text=True)
+  print("\n--- Required Libraries ---\n")
+  print(getReqLibraries.stdout)
 
 
 # 9 System Calls Performed when Run
-runFileFormat = "./{}".format(aFile)
-getSysCalls = subprocess.run(["strace", runFileFormat], capture_output=True, check=True, text=True)
-sysCallsText = getSysCalls.stdout
-print(sysCallsText, file=open("systemCallTrace.txt", "w"))
-print("\n--- System Calls ---\n")
-print(getSysCalls.stderr)
+def get_File_System_Calls(aFile):
+  runFileFormat = "./{}".format(aFile)
+  getSysCalls = subprocess.run(["strace", runFileFormat], capture_output=True, check=True, text=True)
+  sysCallsText = getSysCalls.stdout
+  print(sysCallsText, file=open("systemCallTrace.txt", "w"))
+  print("\n--- System Calls ---\n")
+  print(getSysCalls.stderr)
 
 
 # 10 Library Calls Performed when Run
-getLibCalls = subprocess.run(["ltrace", runFileFormat], capture_output=True, check=True, text=True)
-libCallsText = getLibCalls.stdout
-print(libCallsText, file=open("libCallsTrace.txt", "w"))
-print("\n--- Library Calls ---\n")
-print(getLibCalls.stderr)
+def get_File_Library_Calls(aFile):
+  runFileFormat = "./{}".format(aFile)
+  getLibCalls = subprocess.run(["ltrace", runFileFormat], capture_output=True, check=True, text=True)
+  libCallsText = getLibCalls.stdout
+  print(libCallsText, file=open("libCallsTrace.txt", "w"))
+  print("\n--- Library Calls ---\n")
+  print(getLibCalls.stderr)
 
 
 # 11 How the system and library calls differ, and why
@@ -168,30 +192,28 @@ print(getLibCalls.stderr)
 # 16 What Happens Differently Between the First and Second times 'printf' is Called
 
 
+# Main
+def main(argv):
+  aFile = ""
+  parser = argparse.ArgumentParser(description='A program for analyzing binaries.')
+  parser.add_argument("--file", default=1, type=str, help="The file name (with extension) you wish to analyze.")
+  
+  args = parser.parse_args()
+  aFile = args.file
 
-# Store Final results as object
-results.append({'FileSize': getFileSize,
-                'OwnerName': ownerName,
-                'GroupID': groupID,
-                'SizeBytes': fileSizeBytes,
-                'LastModifiedMonth': lastModified_Month,
-                'LastModifiedDay': lastModified_Day,
-                'LastModifiedHour': lastModified_Hour,
-                'FileOctal': fileOctal,
-                'FileType': fileType
-                })
+  # call functions
+  get_FileSize(aFile)
+  get_FileAttributes(aFile)
+  get_FilePermissions(aFile)
+  get_FileType(aFile)
+  get_Internal_File_Symbols(aFile)
+  get_FileHexdump(aFile)
+  get_External_File_Symbols(aFile)
+  get_File_Required_Libraries(aFile)
+  get_File_System_Calls(aFile)
+  get_File_Library_Calls(aFile)
 
-"""
-ls … file size and attributes
-lsattr … extended file attributes
-file … file type, may not be exact in all cases
-strings … seemingly-printable strings within file
-od and hexdump … print binary data in readable ways
-nm … file symbol information
-ldd … shared library information
-strace … trace system calls
-ltrace … trace library calls
-readelf … ELF executable information
-objdump … assembly code and more
-ndisasm … also disassembly, but better with unstructured binary code
-"""
+
+# Call main, pass in command line argument given
+if __name__ == "__main__":
+  main(sys.argv[1:])
