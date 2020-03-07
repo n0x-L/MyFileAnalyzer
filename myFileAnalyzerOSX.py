1e13a4b7b3cd8118a8d86c34b4d959e155d30bc1
anleeson <anleeson@ucalgary.ca>
2020-03-07 15:43:35 -0700

    Updating error handling, added to_file function and added verbose option

--- myFileAnalyzerOSX.py
+++ myFileAnalyzerOSX.py
@@ -58,19 +58,22 @@ absoluteNumerics = {'0': 'No Permission',
                     '6':'Read+Write',
                     '7':'Read+Write+Execute'}
 
-
-# 1 Get the File size
-def get_FileSize(aFile):
+# 1 File size
+def get_FileSize(aFile, verbose):
   getFileSize = subprocess.run(["du", "-h", aFile], capture_output=True, check=True, text=True)
   getFileSize = getFileSize.stdout
 
   if aFile in getFileSize:
     getFileSize = getFileSize.replace(aFile, '')
-    print("\n--- File Size ---\n", getFileSize)
 
-# 2 Get File Attributes
+    if verbose:
+      print("\n--- File Size ---\n", getFileSize)
+    else:
+      make_file("file_size", getFileSize)
+
+# 2 File Attributes
 # Owner, Group, Timestamps
-def get_FileAttributes(aFile):
+def get_FileAttributes(aFile, verbose):
   getFileAttributes = subprocess.run(["ls", "-l", aFile], capture_output=True, check=True, text=True)
   fileAttributesList = getFileAttributes.stdout.split()
   ownerName = fileAttributesList[2]
@@ -80,114 +83,169 @@ def get_FileAttributes(aFile):
   lastModified_Day = fileAttributesList[6]
   lastModified_Hour = fileAttributesList[7]
 
-  print("--- File Attributes ---")
-  print("Owner Name:", ownerName)
-  print("Group Name or ID:", groupID)
-  print("Last Modified:", lastModified_Month, lastModified_Day, lastModified_Hour)
+  if verbose:
+    print("--- File Attributes ---")
+    print("Owner Name:", ownerName)
+    print("Group Name or ID:", groupID)
+    print("Last Modified:", lastModified_Month, lastModified_Day, lastModified_Hour)
+  else:
+    attr_dict = {"Owner" : ownerName, "Group Name or ID" : groupID, "Last Modified Month" : lastModified_Month, "Last Modified Day" : lastModified_Day, "Last Modified Hour" : lastModified_Hour}
+    make_file("file_attributes", attr_dict)
 
 # Permissions
-def get_FilePermissions(aFile):
+def get_FilePermissions(aFile, verbose):
   getFilePermOctal = subprocess.run(["stat", "-f", "'%A'", aFile], capture_output=True, check=True, text=True)
   fileOctal = getFilePermOctal.stdout
   owner = fileOctal[1]
   group = fileOctal[2]
   other = fileOctal[3]
 
-  print("Owner Permissions:", absoluteNumerics[owner])
-  print("Group Permissions:", absoluteNumerics[group])
-  print("All Others Permissions:", absoluteNumerics[other])
+  if verbose:
+    print("\n--- File Permissions ---")
+    print("Owner Permissions:", absoluteNumerics[owner])
+    print("Group Permissions:", absoluteNumerics[group])
+    print("All Others Permissions:", absoluteNumerics[other])
+  
+  else:
+    perm_dict = {"Owner" : absoluteNumerics[owner], "Group" : absoluteNumerics[group], "Others" : absoluteNumerics[other]}
+    make_file("file_permissions", perm_dict)
 
-# Extended attributes
+# Extended File Attributes
 # Trying to use lsattr: lsattr: Inappropriate ioctl for device While reading flags on a.out
-def get_File_Extended_Attributes(aFile):
+def get_File_Extended_Attributes(aFile, verbose):
   getExtendedAttr = subprocess.run(["xattr", aFile], capture_output=True, check=True, text=True)
   getExtendedAttr = getExtendedAttr.stdout
-  print('\n--- Extended File Attributes ---')
-  if getExtendedAttr:
+
+  if verbose:
+    print('\n--- Extended File Attributes ---')
+    if getExtendedAttr:
       print(getExtendedAttr)
-  else:
+    else:
       print('None.')
 
-# 4 Get the File Type
-def get_FileType(aFile):
-  getFileType = subprocess.run(["file", "-b", aFile], capture_output=True, check=True, text=True)
-  print("\n--- File Type ---", '\n', getFileType.stdout)
+  else:
+    if getExtendedAttr:
+      make_file("extended_attributes", getExtendedAttr)
+    else:
+      make_file("extended_attributes", "None")
 
+# 4 Get the File Type
+def get_FileType(aFile, verbose):
+  getFileType = subprocess.run(["file", "-b", aFile], capture_output=True, check=True, text=True)
+
+  if verbose:
+    print("--- File Type ---\n", getFileType.stdout)
+  else:
+    make_file("file_type", getFileType.stdout)
 
 # 5 Interesting strings within the file
 # use option '-el' to get unicode strings
-def get_FileStrings(aFile):
+def get_FileStrings(aFile, verbose):
   getASCII = subprocess.run(["strings", "-a", aFile], capture_output=True, check=True, text=True)
   stringFileText = getASCII.stdout
-  print(stringFileText, file=open("fileStrings.txt", "w"))
-  print("\n--- Extracted ASCII Strings ---\n")
-  print("FILE CREATED: fileStrings.txt")
 
+  if verbose:
+    print("--- Extracted ASCII Strings ---")
+    print("output too large, sending to file:")
+  
+  make_file("asciiStrings", stringFileText)
 
 # 6 Symbols Defined in the Executable
-def get_Internal_File_Symbols(aFile):
+def get_Internal_File_Symbols(aFile, verbose):
   getSymbols = subprocess.run(["nm", aFile], capture_output=True, check=True, text=True)
   symbolText = getSymbols.stdout
-  print("\n--- Symbols Defined in Executable ---\n")
-  print(symbolText)
+
+  if verbose:
+    print("\n--- Symbols Defined in Executable ---", symbolText)
+  else:
+    make_file("symbols", symbolText)
 
 # Get hexdump
-def get_FileHexdump(aFile):
+def get_FileHexdump(aFile, verbose):
   getHexDump = subprocess.run(["hexdump", "-C", aFile], capture_output=True, check=True, text=True)
   hexFileText = getHexDump.stdout
-  print(hexFileText, file=open("hexDump.txt", "w"))
-  print("\n--- HexDump ---\n")
-  print("FILE CREATED: hexDump.txt\n")
 
+  if verbose:
+    print("\n--- HexDump ---\n output too large, sending to file:")
+  
+  make_file("hexDump", hexFileText)
 
 # 7 Symbols Imported into the Executable
 # display only external symbols of executable
-def get_External_File_Symbols(aFile):
+def get_External_File_Symbols(aFile, verbose):
   getExternalSymbols = subprocess.run(["nm", "-g", aFile], capture_output=True, check=True, text=True)
   externalSymbols = getExternalSymbols.stdout
-  print(externalSymbols, file=open("externalSymbols.txt", "w"))
-  print("\n--- External Symbols ---\n")
-  print("FILE CREATED: externalSymbols.txt\n")
 
+  if verbose:
+    print("\n--- External Symbols ---\n output too large, sending to file")
 
-# 8 Required Libraries
+  make_file("externalSymbols", externalSymbols)
+
+# 8 Required Libraries - *NOT WORKING FOR OSX YET*
 # otool -L
-def get_File_Required_Libraries(aFile):
-  print("\n--- Required Libraries ---\n")
-
+def get_File_Required_Libraries(aFile, verbose):
   try:
     getReqLibraries = subprocess.run(["ldd", "-v", aFile], capture_output=True, check=True, text=True)
-    print(getReqLibraries.stdout)
+    if verbose:
+        print("\n--- Required Libraries ---\n")
+        print(getReqLibraries.stdout)
+    
+    else:
+      make_file("required_libs", getReqLibraries.stdout)
+
   except subprocess.CalledProcessError as er:
     print(er)
 
-  except FileNotFoundError as e:
-    print("Error")
-    print(e)
-    
+  except FileNotFoundError as f:
+    print("\n Problem getting required libraries:")
+    print(f)
 
 # 9 System Calls Performed when Run
 # to trace all system calls (strace) involving memory mapping: sudo strace -q -e trace=memory df -h
 # trace all system calls (strace) involving process mgmt: sudo strace -q -e trace=process df -h	
 # 
 # what is the c start up, what is dynamic linker, and what is the 'main' execution (entry point, (_start))
-def get_File_System_Calls(aFile):
+def get_File_System_Calls(aFile, verbose):
   runFileFormat = "./{}".format(aFile)
-  getSysCalls = subprocess.run(["strace", "-c", runFileFormat], capture_output=True, check=True, text=True)
-  sysCallsText = getSysCalls.stdout
-  print(sysCallsText, file=open("systemCallTrace.txt", "w"))
-  print("\n--- System Calls ---\n")
-  print(getSysCalls.stderr)
+
+  try:
+    getSysCalls = subprocess.run(["strace", "-c", runFileFormat], capture_output=True, check=True, text=True)
+    sysCallsText = getSysCalls.stdout
+
+    if verbose:
+      print("\n--- System Calls ---\n")
+      print(getSysCalls.stderr)
+    else:
+      make_file("system_calls", sysCallsText)
+
+  except subprocess.CalledProcessError as er:
+    print(er)
+
+  except FileNotFoundError as f:
+    print("\n Problem getting system calls:")
+    print(f)
 
 
 # 10 Library Calls Performed when Run
-def get_File_Library_Calls(aFile):
+def get_File_Library_Calls(aFile, verbose):
   runFileFormat = "./{}".format(aFile)
-  getLibCalls = subprocess.run(["ltrace", runFileFormat], capture_output=True, check=True, text=True)
-  libCallsText = getLibCalls.stdout
-  print(libCallsText, file=open("libCallsTrace.txt", "w"))
-  print("\n--- Library Calls ---\n")
-  print(getLibCalls.stderr)
+
+  try:
+    getLibCalls = subprocess.run(["ltrace", runFileFormat], capture_output=True, check=True, text=True)
+    libCallsText = getLibCalls.stdout
+
+    if verbose:
+      print("\n--- Library Calls ---\n")
+      print(getLibCalls.stderr)
+    else:
+      make_file("library_calls", libCallsText)
+
+  except subprocess.CalledProcessError as er:
+    print(er)
+
+  except FileNotFoundError as f:
+    print("\nProblem getting library calls")
+    print(f)
 
 
 # 11 How the system and library calls differ, and why
@@ -216,7 +274,11 @@ def get_System_Library_Delta(aFile):
 
 # 16 What Happens Differently Between the First and Second times 'printf' is Called
 
-
+# Method for creating file output when verbosity is turned off
+def make_file(file_name, content):
+  file_name = file_name + ".txt"
+  print(content, file=open(file_name, "w"))
+  print("\nFile created: ", file_name)
 
 def print_welcome():
   print("\n\t\t\t###########################################################")
@@ -235,32 +297,38 @@ def exit():
 # Main
 def main(argv):
   aFile = ""
+  verbose = False
+
   parser = argparse.ArgumentParser(description='A simply python program for analyzing binaries.')
   parser.add_argument("-f", "--file", required=True, type=str, help="The file name (with extension) you wish to analyze.")
+  parser.add_argument("-v", "--verbose", type=bool, help="Set to 1 for True, False by default.")
   args = parser.parse_args()
   aFile = args.file
+  verbose = args.verbose
 
   # print welcome (really its for keeping track of output)
   print_welcome()
 
   # call functions
-  get_FileSize(aFile)
-  get_FileAttributes(aFile)
-  get_FilePermissions(aFile)
-  get_File_Extended_Attributes(aFile)
-  get_FileType(aFile)
-  get_Internal_File_Symbols(aFile)
-  get_FileHexdump(aFile)
-  get_External_File_Symbols(aFile)
+  get_FileSize(aFile, verbose)
+  get_FileAttributes(aFile, verbose)
+  get_FilePermissions(aFile, verbose)
+  get_File_Extended_Attributes(aFile, verbose)
+  get_FileType(aFile, verbose)
+  get_FileStrings(aFile, verbose)
+  get_Internal_File_Symbols(aFile, verbose)
+  get_FileHexdump(aFile, verbose)
+  get_External_File_Symbols(aFile, verbose)
 
   # Commands not working:
   # ldd, strace, and ltrace not built for OSX
-  get_File_Required_Libraries(aFile)
-  #get_File_System_Calls(aFile)
-  #get_File_Library_Calls(aFile)
+  get_File_Required_Libraries(aFile, verbose)
+  get_File_System_Calls(aFile, verbose)
+  get_File_Library_Calls(aFile, verbose)
 
   # Not implemented yet:
   #get_System_Library_Delta(aFile)
+
   exit()
 
 
